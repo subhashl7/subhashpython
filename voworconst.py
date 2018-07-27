3d3cec65f10d79ffe7e125be8ed214993270e7f6
subhashl7 <41772098+subhashl7@users.noreply.github.com>
2018-07-27 15:52:21 +0530

    Update and rename voworcons.py to voworconst.py

--- voworconst.py
+++ voworconst.py
@@ -2,9 +2,9 @@
 yy=raw_input()
 if((yy>='a' and yy<='z') or (yy>='A' and yy<='Z')):
          if (yy in ['a','e','i','o','u','A','E','I','O','U']):
-                 print('vowel')
+                 print('Vowel')
          else:
-                 print('consonant')
+                 print('Consonant')
 else:
 	print('invalid')
   
