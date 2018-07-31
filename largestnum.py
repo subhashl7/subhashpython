b7ce881f81e4d6180320df154d6fb7cdb41212fc
subhashl7 <41772098+subhashl7@users.noreply.github.com>
2018-07-31 13:12:31 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,10 @@
+#subbu#largestnumber
+a = raw_input
+b = raw_input
+c = raw_input
+if(a>b and a>c):
+	ptint('a is largest num')
+elif(b>a and b>c):
+	print('b is largest num')
+else:
+	print('c is largest num')
