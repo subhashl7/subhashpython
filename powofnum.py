497c3810f94441b629ed353f713c303a4b5665d3
subhashl7 <41772098+subhashl7@users.noreply.github.com>
2018-08-08 15:40:05 +0530

    Update and rename powofnum.py to powofnumb.py

--- powofnum.py
+++
@@ -1,5 +0,0 @@
-#subbu#power of number:
-a,b=input().split()
-a,b=int(a),int(b)
-print(a**b)
-
---
+++ powofnumb.py
@@ -0,0 +1,5 @@
+#subbu#power of number:
+a,b=map(int,raw_input().split())
+n=int(a)**int(b)
+print(n)
+
