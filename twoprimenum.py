f2bbfb044ec45a26a3c4986424f5318b18acd38a
subhashl7 <41772098+subhashl7@users.noreply.github.com>
2018-08-09 15:17:28 +0530

    Create twoprimenum.py

---
+++ twoprimenum.py
@@ -0,0 +1,9 @@
+#subbu#primenum betwn two int:
+a,b = map(int,input().split())  
+for num in range(a,b+1):  
+   if num > 1:  
+       for i in range(2,num):  
+           if (num % i) == 0:  
+               break  
+       else:  
+           print (num)
