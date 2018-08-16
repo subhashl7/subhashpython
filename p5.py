4ffd22577b0512be1b55f583ff2b6ed730b68f8a
subhashl7 <41772098+subhashl7@users.noreply.github.com>
2018-08-16 12:05:15 +0530

    Update p5.py

--- p5.py
+++ p5.py
@@ -1,9 +1,9 @@
-a={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-b=[str(x)for x in input()]
+s={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+r=[str(x)for x in input()]
 sum=0
-for i in range(0,len(b)):
-    if i>0 and a[b[i]] > a[b[i-1]]:
-        sum=a[b[i]]-a[b[i-1]]
+for i in range(0,len(r)):
+    if i>0 and s[r[i]] > s[r[i-1]]:
+        sum=s[r[i]]-s[r[i-1]]
     else:
-        sum+=a[b[i]]
+        sum+=s[r[i]]
 print(sum)
